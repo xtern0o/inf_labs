ddcf9dc90b9eaed2fb68113361322578c900855a
xtern0o <dos30028@gmail.com>
2024-11-15 15:03:31 +0300

    [feat] added 1-line list reading and blocks

--- lab4/task1/main.py
+++ lab4/task1/main.py
@@ -1,6 +1,12 @@
 from pprint import *
 
 
+__all__ = [
+    "parse_object", 
+    "yaml_to_dict",
+]
+
+
 def parse_key(s):
     """подходит для строк вида `key:` и `key` """
     key = s
@@ -28,7 +34,7 @@ def parse_key_value(s):
             value = True
         elif value == "false":
             value = False
-        elif value == "None":
+        elif value == "null":
             value = None
 
         key = parse_key(key)
@@ -52,12 +58,34 @@ def parse_object(lines: list, current_indent: int, current_i: int=0):
             i += 1
             continue
         
-        line = lines[i]
+        line = lines[i].rstrip()
         lstrip_line = line.lstrip()
         current_indent = len(line) - len(lstrip_line)
         if lstrip_line.startswith("-"):
             current_indent += 2
+        
+        # чтение литеральных и сглаженных блоков
+        # работает упрощенно: сколь угодно отступов не сделай, а пробелы в начале строк не считает
+        if lstrip_line[-1] in ("|", ">") and i + 1 < len(lines) and lstrip_line[0] != "-":
+            block_data = []
+            k = i + 1
+            while k < len(lines):
+                if len(lines[k]) - len(lines[k].lstrip()) > current_indent:
+                    block_data.append(lines[k].strip())
+                    parsed_numbers.add(current_i + k)
+                    k += 1
+                else:
+                    break
+            if lstrip_line[-1] == "|":
+                block_text = "\n".join(block_data)
+            else:
+                block_text = " ".join(block_data)
+            key = lstrip_line[:-3]
+            obj_data[key] = block_text
 
+            parsed_numbers.add(current_i + i)
+            continue
+                
         # кончается на :
         if lstrip_line.endswith(":"):
             key = lstrip_line[:-1]
@@ -76,7 +104,7 @@ def parse_object(lines: list, current_indent: int, current_i: int=0):
             if isinstance(obj_data, dict):
                 obj_data = []
             key, value = parse_key_value(lstrip_line[2:])
-            if key is not None and value is not None:
+            if key is not None or value is not None:
                 changed_lines = lines.copy()
                 changed_lines[i] = " " * current_indent + lstrip_line[2:]
                 obj_data.append(parse_object(changed_lines[i:], current_indent, i + current_i))
@@ -89,6 +117,13 @@ def parse_object(lines: list, current_indent: int, current_i: int=0):
         # ключ-значение
         else:
             key, value = parse_key_value(lstrip_line)
+
+            # чтение однострочных последовательностей
+            if isinstance(value, str):
+                if value == "[" + value[1:-1] + "]":
+                    value = value[1:-1].split(", ")
+                
+
             if isinstance(obj_data, dict):
                 obj_data[key] = value
             else:
@@ -142,7 +177,7 @@ def yaml_to_dict(s):
 
 if __name__ == "__main__":
 
-    with open("data/schedule.yaml", mode="r", encoding="utf-8") as in_file:
+    with open("data/example.yaml", mode="r", encoding="utf-8") as in_file:
         yaml_string = in_file.read()
     
     while "\n\n" in yaml_string:
